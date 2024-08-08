881d24a9f6601a1dd26374ae2630315eaf1ead1a
Marcin Baran <mrevib@gmail.com>
2024-08-08 21:02:10 +0200

    Created register/login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     */
+    #[Route('/', name: 'app_homepage')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
